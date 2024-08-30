86d7d25bc0c465cd30fbcb90e186f7dfe52e0b99
Fazliddin Tokhirov <fff@MacBook-Pro-FFF.local>
2024-08-30 11:25:43 +0500

    Toshken time add

--- src/main/java/uz/zafar/primetech/PrimeTechApplication.java
+++ src/main/java/uz/zafar/primetech/PrimeTechApplication.java
@@ -3,9 +3,14 @@ package uz.zafar.primetech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PrimeTechApplication {
 
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tashkent"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeTechApplication.class, args);
 	}
